40fb69745067126ceb2acdca40f7bb4b8c499530
axelfoltyn <axel.foltyn@gmail.com>
2021-04-28 20:54:47 +0200

    ajout des correction config

--- src/main/java/eu/su/mas/dedaleEtu/princ/ConfigurationFile.java
+++ src/main/java/eu/su/mas/dedaleEtu/princ/ConfigurationFile.java
@@ -59,13 +59,13 @@ public final class ConfigurationFile {
 	/**
 	 * Not null when the environment is loaded; should be null if the environment is generated or already online
 	 */
-	public static String INSTANCE_TOPOLOGY=null;
+	//public static String INSTANCE_TOPOLOGY=null;
 	
 	//public static String INSTANCE_TOPOLOGY="resources/topology/map2020-topologyExam1-graph.dgs";
 	//public static String INSTANCE_TOPOLOGY="resources/topology/HouatTopology";
 	//public static String INSTANCE_TOPOLOGY=null;
 	//public static String INSTANCE_TOPOLOGY="resources/topology/map2020-topologyExam1-tree.dgs";
-	//public static String INSTANCE_TOPOLOGY="resources/interlocking/mapInterlocking2-topology";
+	public static String INSTANCE_TOPOLOGY="resources/topology/mapInterlocking2-topology";
 	//public static String INSTANCE_TOPOLOGY="resources/map2018-topology";
 	//public static String INSTANCE_TOPOLOGY="resources/map2019-topologyExam1";
 
@@ -77,11 +77,12 @@ public final class ConfigurationFile {
 	//public static String INSTANCE_CONFIGURATION_ELEMENTS=null;
 	
 	// If the environment is loaded but you do not want to define elements on the map
-	public static String INSTANCE_CONFIGURATION_ELEMENTS=null;
+	
+	//public static String INSTANCE_CONFIGURATION_ELEMENTS=null;
 	//public static String INSTANCE_CONFIGURATION_ELEMENTS="resources/treasureHunt/map2019-elementsExam1";
 	//public static String INSTANCE_CONFIGURATION_ELEMENTS="resources/treasureHunt/Houat-elements";
 	//public static String INSTANCE_CONFIGURATION_ELEMENTS="resources/treasureHunt/map2018-elements-ica";
-	//public static String INSTANCE_CONFIGURATION_ELEMENTS="resources/interlocking/mapInterlocking2-elements";
+	public static String INSTANCE_CONFIGURATION_ELEMENTS="resources/interlocking/mapInterlocking2-elements";
 	//public static String INSTANCE_CONFIGURATION_ELEMENTS="resources/map2018-elements";
 	//public static String INSTANCE_CONFIGURATION_ELEMENTS="resources/mapInterlocking2-elements";
 	
